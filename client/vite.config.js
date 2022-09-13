71b87af9f9d6653557ddfcd846a90bcb2045cb2a
Wajeeh-Haider <swajeeh03@gmail.com>
2022-09-13 06:32:26 -0700

    change the origin

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   proxy: {
     "/": {
-      target: "http://127.0.0.1:4000:5173",
+      target: "http://127.0.0.1:4000",
       changeOrigin: true,
       ws: true,
       credentials: "include",
